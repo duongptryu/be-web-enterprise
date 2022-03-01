efa866fb3ee03b209fcda565d3c76b108a22e01c
duongptryu <duongptryu@gmail.com>
2022-03-01 17:20:38 +0700

    duongpt - Update

--- deploy/stg.sh
+++ deploy/stg.sh
@@ -3,11 +3,11 @@ APP_NAME=webbe
 docker load -i ${APP_NAME}.tar
 docker rm -f ${APP_NAME}
 
-docker run -d --name webbe \
+docker run -d --name ${APP_NAME} \
   -e VIRTUAL_HOST="groupbar.me" \
   -e LETSENCRYPT_HOST="groupbar.me" \
   -e LETSENCRYPT_EMAIL="duongptryu@gmail.com" \
   --net my-net \
   -p 8080:8080 \
   --expose 8080 \
-  webbe
+  ${APP_NAME}
